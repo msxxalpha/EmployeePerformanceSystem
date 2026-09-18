8167394651accc664af87455c3f9906c1965e25f
msxxalpha <alirezaaminfallah@gmail.com>
2026-09-18 12:23:09 +0330

    Fix AppUser table mapping and decimal precision

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -2,5 +2,19 @@ using Microsoft.EntityFrameworkCore;
 namespace Indamin.Performance.Data;
 public class AppDbContext(DbContextOptions<AppDbContext> options):DbContext(options){
  public DbSet<AppUser> Users=>Set<AppUser>();public DbSet<OrgUnit> OrgUnits=>Set<OrgUnit>();public DbSet<Position> Positions=>Set<Position>();public DbSet<Question> Questions=>Set<Question>();public DbSet<Employee> Employees=>Set<Employee>();public DbSet<EvaluationPeriod> Periods=>Set<EvaluationPeriod>();public DbSet<Evaluation> Evaluations=>Set<Evaluation>();public DbSet<EvaluationScore> Scores=>Set<EvaluationScore>();public DbSet<EvaluationScoreHistory> ScoreHistory=>Set<EvaluationScoreHistory>();public DbSet<EvaluatorChangeHistory> EvaluatorHistory=>Set<EvaluatorChangeHistory>();public DbSet<AuditLog> AuditLogs=>Set<AuditLog>();
- protected override void OnModelCreating(ModelBuilder b){b.Entity<AppUser>().HasIndex(x=>x.UserName).IsUnique();b.Entity<Employee>().HasIndex(x=>x.PersonnelNo).IsUnique();b.Entity<Employee>().HasOne(x=>x.Position).WithMany().HasForeignKey(x=>x.PositionId).OnDelete(DeleteBehavior.Restrict);b.Entity<Employee>().HasOne(x=>x.Unit).WithMany().HasForeignKey(x=>x.UnitId).OnDelete(DeleteBehavior.Restrict);b.Entity<Question>().HasOne<Position>().WithMany(x=>x.Questions).HasForeignKey(x=>x.PositionId).OnDelete(DeleteBehavior.Cascade);b.Entity<Evaluation>().HasIndex(x=>new{x.PeriodId,x.EmployeeId}).IsUnique();b.Entity<EvaluationScore>().HasIndex(x=>new{x.EvaluationId,x.QuestionId}).IsUnique();}
+ protected override void OnModelCreating(ModelBuilder b){
+  b.Entity<AppUser>().ToTable("AppUsers");
+  b.Entity<AppUser>().HasIndex(x=>x.UserName).IsUnique();
+  b.Entity<Employee>().HasIndex(x=>x.PersonnelNo).IsUnique();
+  b.Entity<Employee>().HasOne(x=>x.Position).WithMany().HasForeignKey(x=>x.PositionId).OnDelete(DeleteBehavior.Restrict);
+  b.Entity<Employee>().HasOne(x=>x.Unit).WithMany().HasForeignKey(x=>x.UnitId).OnDelete(DeleteBehavior.Restrict);
+  b.Entity<Question>().HasOne<Position>().WithMany(x=>x.Questions).HasForeignKey(x=>x.PositionId).OnDelete(DeleteBehavior.Cascade);
+  b.Entity<Evaluation>().HasIndex(x=>new{x.PeriodId,x.EmployeeId}).IsUnique();
+  b.Entity<EvaluationScore>().HasIndex(x=>new{x.EvaluationId,x.QuestionId}).IsUnique();
+  b.Entity<Position>().Property(x=>x.MaxScore).HasPrecision(10,2);
+  b.Entity<Question>().Property(x=>x.MaxScore).HasPrecision(10,2);
+  b.Entity<EvaluationScore>().Property(x=>x.Score).HasPrecision(10,2);
+  b.Entity<EvaluationScoreHistory>().Property(x=>x.OldScore).HasPrecision(10,2);
+  b.Entity<EvaluationScoreHistory>().Property(x=>x.NewScore).HasPrecision(10,2);
+ }
 }
