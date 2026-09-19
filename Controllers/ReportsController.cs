f9e4fdde446b8a5df5bf4d5c047005239e25c2ad
msxxalpha <alirezaaminfallah@gmail.com>
2026-09-19 12:30:57 +0330

    Fix management reports ExcelService namespace import

--- Controllers/ReportsController.cs
+++ Controllers/ReportsController.cs
@@ -1,4 +1,5 @@
 using Indamin.Performance.Data;
+using Indamin.Performance.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
